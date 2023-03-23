2023d111fb30829ece71e429df2c11b08d3b56bd
SvyatGysto <svyatgysto@gmail.com>
2023-03-23 17:55:05 +0300

    Task three

---
+++ Task_Three/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if(num1%2 ==0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
